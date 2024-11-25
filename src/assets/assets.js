72b7ae926a800182fd33a8c6a5349b1d131105bf
Adesh-111 <adeshabinesh25@gmail.com>
2024-11-25 17:58:58 +0530

    LoginPage component 100% completed

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,8 +1,14 @@
 import logo from "./logo.svg";
-import background from "./background.png"
+import background from "./background.png";
+import google from "./Google.svg"
+import x from "./X.svg"
+import facebook from "./Facebook.svg"
 
 const assets = {
-    logo
+    logo,
+    google,
+    facebook,
+    x
 };
 
 export default assets;
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -46,91 +46,102 @@ const LoginPage = () => {
     alert("Login Successful");
   };
   return (
-    
     <div className="auth-container">
-        <div className="main-auth-box">
-      <div className="auth-box signup-form">
-        <h2>Hey there!</h2>
-        <form onSubmit={handleSignupSubmit}>
-          <label style={{color: "#30C70F"}}>Email:</label>
-          <input
-            type="email"
-            name="email"
-            className="email"
-            value={signupData.email}
-            onChange={handleSignupChange}
-            required
-          />
+      <div className="main-auth-box">
+        <div className="auth-box signup-form">
+          <h2>Hey there!</h2>
+          <form onSubmit={handleSignupSubmit}>
+            <label style={{ color: "#30C70F" }}>Email:</label>
+            <input
+              type="email"
+              name="email"
+              className="email"
+              value={signupData.email}
+              onChange={handleSignupChange}
+              required
+            />
 
-          <label style={{color: "#1B82B3"}}>Password:</label>
-          <input
-          className="pass"
-            type="password"
-            name="password"
-            value={signupData.password}
-            onChange={handleSignupChange}
-            
-            required
-          />
+            <label style={{ color: "#1B82B3" }}>Password:</label>
+            <input
+              className="pass"
+              type="password"
+              name="password"
+              value={signupData.password}
+              onChange={handleSignupChange}
+              required
+            />
 
-          <label>Confirm Password:</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            className="confirmPass"
-            value={signupData.confirmPassword}
-            onChange={handleSignupChange}
-            required
-            style={{ borderColor: error ? "red" : "grey" ,
-                boxShadow : error ? " 5px 5px 15px 1px red" :  "none",
-            }}
-          />
-          {error && <p className="error-text">{error}</p>}
+            <label>Confirm Password:</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              className="confirmPass"
+              value={signupData.confirmPassword}
+              onChange={handleSignupChange}
+              required
+              style={{
+                borderColor: error ? "red" : "grey",
+                boxShadow: error ? " 5px 5px 15px 1px red" : "none",
+              }}
+            />
+            {error && <p className="error-text">{error}</p>}
 
-          <label className="terms">
-            <input type="checkbox" required className="check"/>I hereby agree to all the Terms
-            and Conditions mentioned <a href="#">here</a>
-          </label>
+            <label className="terms">
+              <input type="checkbox" required className="check" />I hereby agree
+              to all the Terms and Conditions mentioned <a href="#">here</a>
+            </label>
 
-          <button type="submit" className="sign-up-button">Sign In</button>
-        </form>
-      </div>
-
-      <div className="vertical-line">|</div>
-
-      <div className="auth-box login-form">
-        <h2>Welcome back!</h2>
-        <form onSubmit={handleLoginSubmit}>
-          <label style={{color: "#30C70F"}}>Email:</label>
-          <input
-            type="email"
-            name="email"
-            className="email"
-            value={loginData.email}
-            onChange={handleLoginChange}
-            required
-          />
-
-          <label style={{color: "#1B82B3"}}>Password:</label>
-          <input
-            type="password"
-            name="password"
-            className="password"
-            value={loginData.password}
-            onChange={handleLoginChange}
-            required
-          />
-          <a href="#" className="forgot-password">Forgot Password?</a>
-
-          <button type="submit" className="login-button">Log In</button>
-        </form>
-        <p style={{color: "grey"}}>or you can use</p>
-        <div className="social-buttons">
-          <a href="#">{assets.}</a>
-<a href="#">{assets}</a>
-      <a href="#">{assets}</a>    
+            <button type="submit" className="sign-up-button">
+              Sign In
+            </button>
+          </form>
         </div>
-      </div>
+
+        <div className="vertical-line">|</div>
+
+        <div className="auth-box login-form">
+          <h2>Welcome back!</h2>
+          <form onSubmit={handleLoginSubmit}>
+            <label style={{ color: "#30C70F" }}>Email:</label>
+            <input
+              type="email"
+              name="email"
+              className="email"
+              value={loginData.email}
+              onChange={handleLoginChange}
+              required
+            />
+
+            <label style={{ color: "#1B82B3" }}>Password:</label>
+            <input
+              type="password"
+              name="password"
+              className="password"
+              value={loginData.password}
+              onChange={handleLoginChange}
+              required
+            />
+            <a href="#" className="forgot-password">
+              Forgot Password?
+            </a>
+
+            <button type="submit" className="login-button">
+              Log In
+            </button>
+          </form>
+          <p style={{ color: "grey" }}>or you can use</p>
+          <div className="social-buttons">
+            <a href="#">
+              <img src={assets.google} alt="" />
+            </a>
+            <a href="#">
+              <img src={assets.facebook} alt="" />
+            </a>
+            <a href="#">
+              <img src={assets.x} alt="" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
