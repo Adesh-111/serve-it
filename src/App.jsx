13a61526aa0964766c5b3b495b522b3f634a62d1
Adesh-111 <adeshabinesh25@gmail.com>
2024-12-04 20:10:39 +0530

    Completion of structure of Browse

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import LoginPage from "./components/LoginPage/LoginPage";
 import Navbar from "./components/Navbar/Navbar";
+import Browse from "./components/Browse/Browse";
 
 function App() {
-  return ( <Navbar /> );
+  return (
+    <>
+      <Navbar />
+      <Browse />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Browse/Browse.jsx
@@ -0,0 +1,17 @@
+function Browse() {
+    return ( <>
+    <div className="browse-container">
+        <div className="browse">
+            <div className="browse-top">
+                <div className="search-items">
+                    <button>
+                        <input type="search" name="" id="" />
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </> );
+}
+export default Browse;
