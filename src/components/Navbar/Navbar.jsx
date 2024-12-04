abdbe14388c76c547095b99e56092595cb634cb1
Adesh-111 <adeshabinesh25@gmail.com>
2024-12-04 12:54:20 +0530

    Completion of navbar without responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,8 +27,10 @@ function Navbar() {
           </ul>
           <div className="user-details">
             <img src={assets.demoUser} alt="demo-user" className="user-img" />
-            <h3 className="user-name">Poornima Sundar</h3>
-            <p className="user-place">Poornima's Wedding</p>
+            <div className="user-desc">
+              <h3 className="user-name">Poornima Sundar</h3>
+              <p className="user-place">Poornima's Wedding</p>
+            </div>
           </div>
         </div>
       </div>
